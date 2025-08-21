006323f92a30481818d8e1bdeb5927f7f8c106e3
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-08-21 21:29:52 +0300

    add eslint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig([
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -12,23 +15,36 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        projectService: true,
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
+        sourceType: 'commonjs',
+      },
       globals: {
         ...globals.node,
         ...globals.jest,
+        ...globals.browser,
       },
       sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
+    plugins: {js,'@typescript-eslint': tseslint },
+    extends: [
+      'js/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-type-checked',
+      'plugin:prettier/recommended',
+    ]
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-);
+]);
