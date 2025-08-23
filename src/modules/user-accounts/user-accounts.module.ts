106c1a03aa5031bcefbbd78fe4a772d26ebfddbb
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-08-23 22:02:35 +0300

    add user-accounts.controller.ts

--- src/modules/user-accounts/user-accounts.module.ts
+++ src/modules/user-accounts/user-accounts.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserAccountsController } from './user-accounts.controller';
 
-@Module({})
+@Module({
+  controllers: [UserAccountsController]
+})
 export class UserAccountsModule {}
