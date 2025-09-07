3ecbb12f1d60b827fc19586723dd1458371dd259
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-09-07 18:17:51 +0300

    patch deleteAll methods

--- src/modules/testing/application/usecases/delete-all.usecase.ts
+++ src/modules/testing/application/usecases/delete-all.usecase.ts
@@ -1,11 +1,15 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { TestingRepository } from '../../infrastructure/testing.repository';
 
 export class DeleteAllCommand {
   constructor() {}
 }
 
 @CommandHandler(DeleteAllCommand)
-export class DeleteAllUseCase implements ICommandHandler<DeleteAllCommand>{
-  constructor(private readonly dto: DeleteAllCommand){}
+export class DeleteAllUseCase implements ICommandHandler<DeleteAllCommand> {
+  constructor(private readonly testingRepository: TestingRepository) {}
 
+  async execute(){
+    return await this.testingRepository.deleteAll()
+  }
 }
---
+++ src/modules/testing/infrastructure/testing.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class TestingRepository {
+  constructor(@InjectDataSource() private dataSource: DataSource) {}
+  async deleteAll() {
+    return this.dataSource.query(
+      `
+      TRUNCATE TABLE "Users" CASCADE
+      `,
+    );
+  }
+}
