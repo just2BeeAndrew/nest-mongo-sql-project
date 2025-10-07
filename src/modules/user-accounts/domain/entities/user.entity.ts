d7499df27b6ca607d008ecd9b4e62253cc4fd532
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-10-07 22:32:25 +0300

    update users entity

--- src/modules/user-accounts/domain/entities/user.entity.ts
+++ src/modules/user-accounts/domain/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+@PrimaryGeneratedColumn()
+  public id: string;
+
+}
+
+@Entity()
+export class AccountData{
+@PrimaryGeneratedColumn()
+public id: string;
+
+}
+
+@Entity()
+export class EmailConfirmation{
+  @PrimaryGeneratedColumn()
+  public id: string;
+}
